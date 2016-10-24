5b9625927ed9fbcea2f54d5fc69373d3a08d8b9f
Cristian Velez Ruiz <ainokila@users.noreply.github.com>
2016-10-24 11:44:06 +0200

    Update objeto_rev.cc

--- P2/objeto_rev.cc
+++ P2/objeto_rev.cc
@@ -1,11 +1,25 @@
 #include "objeto_rev.h"
 #include <string>
 #include <vector>
+#include <math.h>
 
-ObjetoRevolucion::ObjetoRevolucion(std::vector<float> puntos){
+ObjetoRevolucion::ObjetoRevolucion(std::vector<float> puntos,float grados){
   puntosIniciales = puntos;
+  generarRevolucion(grados);
 
 }
-ObjetoRevolucion::ObjetoRevolucion(std::string file){
+
+ObjetoRevolucion::ObjetoRevolucion(std::string file,float grados){
   leerPly(file);
+  generarRevolucion(grados);
 }
+
+void ObjetoRevolucion::generarRevolucion(float grados){
+  //triangulos = puntosIniciales;
+
+  for(float i= 0.0f ; i < 360.0f ; i = i+grados){
+    for(int j = 3 ; j < puntosIniciales.size()-3; j = j+3){
+      insertarVertice(puntosIniciales[j]*cos(i),puntosIniciales[j+1],puntosIniciales[j+2]*sin(i));
+    }
+  }
+
