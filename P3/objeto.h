954e508e645a98c08efd06a82030144d6057066a
ainokila <universidadcvr@gmail.com>
2016-11-08 16:42:24 +0100

    Iniciando P3

---
+++ P3/objeto.h
@@ -0,0 +1,45 @@
+#ifndef _MALLA_H
+#define _MALLA_H
+#include<vector>
+#include <GL/gl.h>
+#include "file_ply_stl.h"
+#include "bounding_box.h"
+
+
+typedef GLfloat Vertice[3];
+
+
+class Objeto{
+
+
+  public:
+
+    Objeto();
+    void dibujar();
+
+    inline int getNumVertices(){return puntos.size()/3;};
+    inline int getNumCaras(){return caras.size()/3;};
+
+    void insertarVertice(float x,float y,float z);
+    void cambiarDibujado(int nuevo);
+    void insertarCara(int v1, int v2, int v3);
+
+    inline void setPuntos(std::vector<float> aux){puntos = aux;};
+    inline void setCaras(std::vector<int> aux){caras = aux;};
+    void leerPly();
+
+
+    void ampliar();
+    void reducir();
+
+
+  protected:
+
+    std::vector<float> puntos;
+    std::vector<int> caras;
+    int modoPintado; //0 puntos 1 lineas 2 solido 3 ajedrez
+    GLenum modePolygon;
+    BoundingBox bound;
+};
+
+#endif
